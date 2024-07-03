d55a33fbf852c2aff7b26a7d51a5b83c6994fbc9
GDD-impact <gddimpactsoftwares@gmail.com>
2024-07-03 11:16:58 +0100

    updates

--- whatsappAPI/admin.py
+++ whatsappAPI/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Whatsappuser
 
 # Register your models here.
+@admin.register(Whatsappuser)
+class WhatsappuserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'created_at', 'updated_at')
+    search_fields = ('name', 'phone')
+    list_filter = ('created_at', 'updated_at')
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
