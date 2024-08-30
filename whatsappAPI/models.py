78fc68c1d4cf4d03a043a64bd28d96e9cbaefdc3
GDD-impact <gddimpactsoftwares@gmail.com>
2024-08-30 17:42:03 +0100

    update

--- whatsappAPI/models.py
+++ whatsappAPI/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 from django.utils.dateformat import DateFormat
+from django.conf import settings
+DEBUG_MODE = settings.DEBUG_ENV
 
 class Contact(models.Model):
     wa_id = models.CharField(max_length=50, unique=True)
@@ -35,7 +37,6 @@ class ReceivedMessage(models.Model):
     mime_type = models.CharField(max_length=100, blank=True, null=True) # For media messages
     timestamp = models.DateTimeField(auto_now_add=True)
     message_mode = models.CharField(max_length=20, choices=MESSAGE_MODES, default='received message')
-    seen = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.contact}: {self.message_type}"
@@ -43,46 +44,47 @@ class ReceivedMessage(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         
-        # Fetch all received messages for this contact
-        all_received_messages = list(self.contact.recieved_messages.values(
-            'id', 'message_id', 'message_type', 'body', 'media_id', 'mime_type', 'timestamp', 'message_mode'
-        ))
+        if DEBUG_MODE:
+            # Fetch all received messages for this contact
+            all_received_messages = list(self.contact.recieved_messages.values(
+                'id', 'message_id', 'message_type', 'body', 'media_id', 'mime_type', 'timestamp', 'message_mode'
+            ))
 
-        # Convert the 'timestamp' field from datetime to string
-        for message in all_received_messages:
-            if 'timestamp' in message:
-                message['timestamp'] = DateFormat(message['timestamp']).format('Y-m-d H:i:s')
+            # Convert the 'timestamp' field from datetime to string
+            for message in all_received_messages:
+                if 'timestamp' in message:
+                    message['timestamp'] = DateFormat(message['timestamp']).format('Y-m-d H:i:s')
 
-        # Fetch all sent messages for this contact
-        all_sent_messages = list(self.contact.sent_messages.values(
-            'id', 'message_id', 'message_type', 'body', 'link', 'timestamp', 'message_mode', 'status'
-        ))
-        # Convert the 'timestamp' field from datetime to string
-        for message in all_sent_messages:
-            if 'timestamp' in message:
-                message['timestamp'] = DateFormat(message['timestamp']).format('Y-m-d H:i:s')
+            # Fetch all sent messages for this contact
+            all_sent_messages = list(self.contact.sent_messages.values(
+                'id', 'message_id', 'message_type', 'body', 'link', 'timestamp', 'message_mode', 'status'
+            ))
+            # Convert the 'timestamp' field from datetime to string
+            for message in all_sent_messages:
+                if 'timestamp' in message:
+                    message['timestamp'] = DateFormat(message['timestamp']).format('Y-m-d H:i:s')
 
-        # Prepare the message data
-        received_message_contact = {
-            'id': self.contact.id,
-            'wa_id': self.contact.wa_id,
-            'profile_name': self.contact.profile_name,
-            'recieved_messages': all_received_messages,
-            'sent_messages': all_sent_messages
-        }
-        
-        # Determine the room name
-        room_name = "whatsappapi_general"
-        
-        # Get the channel layer and send the message
-        channel_layer = get_channel_layer()
-        async_to_sync(channel_layer.group_send)(
-            room_name,
-            {
-                'type': 'chat_message',
-                'contact': received_message_contact
+            # Prepare the message data
+            received_message_contact = {
+                'id': self.contact.id,
+                'wa_id': self.contact.wa_id,
+                'profile_name': self.contact.profile_name,
+                'recieved_messages': all_received_messages,
+                'sent_messages': all_sent_messages
             }
-        )
+            
+            # Determine the room name
+            room_name = "whatsappapi_general"
+            
+            # Get the channel layer and send the message
+            channel_layer = get_channel_layer()
+            async_to_sync(channel_layer.group_send)(
+                room_name,
+                {
+                    'type': 'chat_message',
+                    'contact': received_message_contact
+                }
+            )
 
 
 class SentMessage(models.Model):
@@ -94,7 +96,6 @@ class SentMessage(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     message_mode = models.CharField(max_length=20, choices=MESSAGE_MODES, default='sent message')
     status = models.CharField(max_length=20, choices=[("pending","pending"),("sent","sent")], default='pending')  # 'sent', 'delivered', 'read', etc.
-    
 
 
 class Status(models.Model):
