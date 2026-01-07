e954d1ce4818e60767d3ed1d6aa06da6c096e1a5
GDD-impact <gddimpactsoftwares@gmail.com>
2026-01-07 22:42:09 +0100

    update

--- ICCapiservices/settings.py
+++ ICCapiservices/settings.py
@@ -211,7 +211,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 if DEBUG_ENV:
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
@@ -224,8 +223,11 @@ else:
     AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     AWS_S3_OBJECT_PARAMETERS={'CacheControl':'max-age=86400'}
     AWS_S3_REGION_NAME = 'us-east-1'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    DEFAULT_FILE_STORAGE='ICCapiservices.storages.MediaStore'
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_QUERYSTRING_AUTH = False  # Don't add authentication to URLs
+    STATICFILES_STORAGE = 'ICCapiservices.storages.StaticStore'
+    DEFAULT_FILE_STORAGE = 'ICCapiservices.storages.MediaStore'
     AWS_LOCATION = 'static'
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets"),]
     STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
--- ICCapiservices/storages.py
+++ ICCapiservices/storages.py
@@ -1,5 +1,11 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
+class StaticStore(S3Boto3Storage):
+	location = "static"
+	default_acl = 'public-read'
+	file_overwrite = False
+
 class MediaStore(S3Boto3Storage):
-	location = "media"   # ensures prefix is kept
-	file_overwrite=True 
+	location = "media"
+	default_acl = 'public-read'
+	file_overwrite = False  # Changed to False to preserve unique filenames 
